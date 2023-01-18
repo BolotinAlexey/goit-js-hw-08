8715232040697444c69c7be4c7a775c012d75759
Oleksii Bolotin <samkiral73@gmail.com>
2023-01-18 19:09:28 +0100

    add parcel,SimpleLightbox

---
+++ src/js/01-gallery.js
@@ -0,0 +1,30 @@
+import { galleryItems } from './gallery-items.js';
+
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryEl = document.querySelector('.gallery');
+
+createGalleryInDOM();
+
+// config gallery
+const gallerySetting = {
+  captionDelay: 250,
+  captionsData: 'alt',
+};
+
+// on turn library "SimpleLightbox"
+const lightbox = new SimpleLightbox('.gallery a', gallerySetting);
+
+// create HTML markdown of gallery
+function createGalleryInDOM() {
+  const injectHTML = galleryItems.map(createGalleryItem).join('');
+  galleryEl.insertAdjacentHTML('beforeend', injectHTML);
+}
+
+// create a element of gallery
+function createGalleryItem(el) {
+  return `<a class="gallery__item" href= ${el.original}>
+  <img class="gallery__image" src=${el.preview} alt=${el.description} />
+</a>`;
+}
