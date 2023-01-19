627935355e83d0d3f75347630d485bce3f8477aa
Oleksii Bolotin <samkiral73@gmail.com>
2023-01-19 23:18:39 +0100

    finish 2 task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const settings = {
+  url: 'https://player.vimeo.com/video/236203659',
+  width: 640,
+  height: 360,
+  autoplay: true,
+};
+
+const player = new Player('vimeo-player', settings);
+const currentLocalStorageValue = localStorage.getItem(
+  'videoplayer-current-time'
+);
+
+const currentTime = currentLocalStorageValue ? currentLocalStorageValue : 0;
+
+player.setCurrentTime(currentTime);
+player.on('timeupdate', throttle(recordPlayTime, 1000));
+
+function recordPlayTime(e) {
+  localStorage.setItem('videoplayer-current-time', e.seconds);
+}
