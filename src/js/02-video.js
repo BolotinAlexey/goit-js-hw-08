577ac95a561a7b076cd5d463f70760eb907a1a7a
Oleksii Bolotin <samkiral73@gmail.com>
2023-01-20 00:46:41 +0100

    finish 3task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,6 +1,8 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
+const CURRENT_TIME = 'videoplayer-current-time';
+
 const settings = {
   url: 'https://player.vimeo.com/video/236203659',
   width: 640,
@@ -9,15 +11,13 @@ const settings = {
 };
 
 const player = new Player('vimeo-player', settings);
-const currentLocalStorageValue = localStorage.getItem(
-  'videoplayer-current-time'
-);
+const currentLocalStorageValue = localStorage.getItem(CURRENT_TIME);
 
-const currentTime = currentLocalStorageValue ? currentLocalStorageValue : 0;
+const settingTime = currentLocalStorageValue ? currentLocalStorageValue : 0;
 
-player.setCurrentTime(currentTime);
+player.setCurrentTime(settingTime);
 player.on('timeupdate', throttle(recordPlayTime, 1000));
 
 function recordPlayTime(e) {
-  localStorage.setItem('videoplayer-current-time', e.seconds);
+  localStorage.setItem(CURRENT_TIME, e.seconds);
 }
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,32 @@
+import throttle from 'lodash.throttle';
 
+const FORM_KEY = 'feedback-form-state';
+
+const formEl = document.querySelector('.feedback-form');
+const localCurrent = JSON.parse(localStorage.getItem('feedback-form-state'));
+const formData = localCurrent ? localCurrent : {};
+
+formEl.addEventListener('input', throttle(onInput, 500));
+formEl.addEventListener('submit', onSubmitForm);
+
+// init form fields
+for (const el of formEl.elements) {
+  el.value = formData[el.name] ? formData[el.name] : '';
+}
+
+function onInput(e) {
+  formData[e.target.name] = e.target.value;
+  localStorage.setItem(FORM_KEY, JSON.stringify(formData));
+}
+
+function onSubmitForm(e) {
+  e.preventDefault();
+  const { email, message } = e.target.elements;
+
+  if (!(email.value && message.value)) alert('input data');
+  else {
+    console.log({ email: email.value, message: message.value });
+    formEl.reset();
+    localStorage.setItem('feedback-form-state', null);
+  }
+}
